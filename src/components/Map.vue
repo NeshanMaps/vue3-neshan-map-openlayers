f9af5b4ace808ff6e52b96c6442cba9730d0960d
morteza gholami <mortezagholami1376.mg@gmail.com>
2022-06-20 11:23:47 +0430

    feat: add tiles and traffic and poi layer.

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -1,16 +1,80 @@
 <template>
-  <div id="map"></div>
+  <div id="map">
+    <div id="settings">
+      <div id="tiles">
+        <div
+          v-for="tile in tiles"
+          :key="tile.title"
+          @click="changeMapType(tile.title)"
+          class="tile"
+          :class="{ 'selected-tile': tile.title == mapType }"
+        >
+          <img :src="tile.url" />
+          <div class="desc">
+            {{ tile.title }}
+          </div>
+        </div>
+      </div>
+      <div id="checkboxes">
+        <label
+          >poi
+          <input
+            type="checkbox"
+            name="poi"
+            @change="togglePoi(poiLayer)"
+            v-model="poiLayer"
+          />
+        </label>
+        <label for="traffic"
+          >traffic
+          <input
+            type="checkbox"
+            name="traffic"
+            @change="toggleTraffic(trafficLayer)"
+            v-model="trafficLayer"
+          />
+        </label>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
 /* eslint-disable */
+const tiles = [
+  {
+    title: "osm-bright",
+    url: "https://static.neshan.org/sdk/examples/maptypes/images/map-types-bright.png",
+  },
+  {
+    title: "standard-night",
+    url: "https://static.neshan.org/sdk/examples/maptypes/images/map-types-standard-night.png",
+  },
+  {
+    title: "standard-day",
+    url: "https://static.neshan.org/sdk/examples/maptypes/images/map-types-standard-day.png",
+  },
+  {
+    title: "neshan",
+    url: "https://static.neshan.org/sdk/examples/maptypes/images/map-types-neshan.png",
+  },
+  {
+    title: "dreamy-gold",
+    url: "https://static.neshan.org/sdk/examples/maptypes/images/map-types-dreamy-gold.png",
+  },
+  {
+    title: "dreamy",
+    url: "https://static.neshan.org/sdk/examples/maptypes/images/map-types-dreamy.png",
+  },
+];
 const createHeaders = (token) => {
   return {
-    "Api-Key": token
-  }
+    "Api-Key": token,
+  };
 };
 const urls = {
   reverse: "https://api.neshan.org/v4/reverse",
   search: "https://api.neshan.org/v1/search",
+  map: "https://static.neshan.org/sdk/openlayers/5.3.0/ol.js",
 };
 const createApi = (token) => {
   return {
@@ -28,16 +92,15 @@ const createApi = (token) => {
       });
       return await res.json();
     },
-  }
+  };
 };
 const markerUrls = {
   red: "https://img.icons8.com/color/344/marker--v1.png",
   blue: "https://img.icons8.com/ultraviolet/344/marker.png",
 };
 
-const createText = ({ text }) => {
+const createText = () => {
   return new ol.style.Text({
-    text,
     overflow: true,
     scale: 1.6,
     offsetY: -43,
@@ -83,6 +146,20 @@ const createLayer = ({ target = "points", style, source }) => {
     style,
   });
 };
+const getTitleFromData = (data) => {
+  const mainTitle = data.place
+    ? data.place
+    : data.route_name
+    ? data.route_name
+    : "معبر بدون نام";
+  const fullTitle = data.neighbourhood
+    ? mainTitle + `، ${data.neighbourhood}`
+    : mainTitle;
+  return fullTitle;
+};
+const sanitizeLocation = (loc) => {
+  return loc && loc instanceof Object ? [loc.longitude, loc.latitude] : loc;
+};
 export default {
   name: "NeshanMap",
   props: {
@@ -103,15 +180,22 @@ export default {
     return {
       sanitizedCenter: null,
       map: null,
-      markers: [],
       mainMarker: null,
-      api: null
+      mainMarkerCoords: null,
+      searchMarkers: null,
+      api: null,
+      tiles: tiles,
+      mapType: "neshan",
+      poiLayer: this.poi,
+      trafficLayer: this.traffic,
     };
   },
-  methods: {
-    sanitizeLocation(loc) {
-      return loc && loc instanceof Object ? [loc.longitude, loc.latitude] : loc;
+  watch: {
+    serviceKey(nv) {
+      this.setToken(nv);
     },
+  },
+  methods: {
     async getLocation() {
       const position = await new Promise((resolve) => {
         navigator.geolocation.getCurrentPosition((pos) => {
@@ -119,7 +203,7 @@ export default {
         });
       });
       return position
-        ? this.sanitizeLocation(position.coords)
+        ? sanitizeLocation(position.coords)
         : [59.5870851, 36.311559];
     },
     async bundleMap() {
@@ -133,6 +217,7 @@ export default {
         view: new ol.View({
           center: ol.proj.fromLonLat(coords),
           zoom: this.zoom,
+          smoothExtentConstraint: true,
         }),
       });
       this.map = map;
@@ -144,82 +229,94 @@ export default {
     importMap() {
       if (document.getElementById("my-overlayer")) return; // was already loaded
       const scriptTag = document.createElement("script");
-      scriptTag.src = "https://static.neshan.org/sdk/openlayers/5.3.0/ol.js";
+      scriptTag.src = urls.map;
       scriptTag.id = "my-overlayer";
       document.getElementsByTagName("head")[0].appendChild(scriptTag);
       return scriptTag;
     },
-    addMarkers(points, { layer, style, image, color, text, iconScale } = {}) {
+    addMarkers(points) {
+      const [{ layer, style, image, color, iconScale } = {}] = points;
       const features = points.map(
-        (point) => new ol.Feature(new ol.geom.Point(point))
+        (point) =>
+          new ol.Feature({
+            geometry: new ol.geom.Point(point.coords),
+            text: point.text,
+          })
       );
-      const _text = text ? createText({ text }) : undefined;
+      const _text = createText();
       const _image = image || createIcon({ color, iconScale });
       const _style = style || createStyle({ text: _text, image: _image });
+      const styleFunc = (feature) => {
+        _style.getText().setText(feature.get("text"));
+        return _style;
+      };
       const source = createSource(features);
-      const _pinLayer = layer || createLayer({ style: _style, source });
+      const _pinLayer = layer || createLayer({ style: styleFunc, source });
       this.map.addLayer(_pinLayer);
       return { layer: _pinLayer, style: _style };
     },
     async handleClickEvent(event) {
-      this.map.removeLayer(this.mainMarker);
-      const point = event.coordinate;
-      const { layer: marker, style } = this.addMarkers([point], { text: " " });
-      const stdPoint = ol.proj.transform(point, "EPSG:3857", "EPSG:4326");
-      const data = await this.api.REVERSE(...stdPoint);
-      const text = this.getTitleFromData(data);
-      style.getText().setText(text);
-      marker.setStyle(style);
-      this.mainMarker = marker;
-      this.search("", stdPoint);
-      // map.forEachFeatureAtPixel(event.pixel, function (feature, layer) {
-      //   console.log(feature, layer);
-      //   featureListener(event);
-      // });
-    },
-    getTitleFromData(data) {
-      const mainTitle = data.place
-        ? data.place
-        : data.route_name
-          ? data.route_name
-          : "معبر بدون نام";
-      const fullTitle = data.neighbourhood
-        ? mainTitle + `، ${data.neighbourhood}`
-        : mainTitle;
-      return fullTitle;
-    },
-    async search(text, point) {
       try {
-        const result = await this.api.SEARCH("خانه کتاب", point);
-        this.markers = this.markers.filter((marker) => {
-          this.map.removeLayer(marker);
-          return false;
-        });
-        result.items.forEach((item) => {
-          const coords = Object.values(item.location);
-          const stdPoint = ol.proj.transform(coords, "EPSG:4326", "EPSG:3857");
-          const { layer: marker, style } = this.addMarkers([stdPoint], {
+        this.map.removeLayer(this.mainMarker);
+        const point = event.coordinate;
+        const { layer: marker, style } = this.addMarkers([
+          { coords: point, text: " " },
+        ]);
+        const stdPoint = ol.proj.transform(point, "EPSG:3857", "EPSG:4326");
+        const data = await this.api.REVERSE(...stdPoint);
+        const text = getTitleFromData(data);
+        style.getText().setText(text);
+        marker.setStyle(style);
+        this.mainMarker = marker;
+        this.$emit("click", { event, marker, stdPoint, data });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+    async search({ text = "", coords }) {
+      try {
+        coords ||= this.mainMarkerCoords || this.sanitizedCenter;
+        const result = await this.api.SEARCH(text, coords);
+        this.map.removeLayer(this.searchMarkers);
+        this.searchMarkers = null;
+        const points = result.items.map((item) => {
+          const point = Object.values(item.location);
+          const stdPoint = ol.proj.transform(point, "EPSG:4326", "EPSG:3857");
+          return {
+            coords: stdPoint,
+            text: item.title,
             color: "blue",
-            text: " ",
             iconScale: 0.07,
-          });
-          style.getText().setText(item.title);
-          marker.setStyle(style);
-          this.markers.push(marker);
+          };
         });
-        // this.map.panTo(yourVectorLayer.getDataExtent().getCenterLonLat());
-        // this.map.zoomTo(yourVectorLayer.getDataExtent().getZoomExtent());
+        const { layer } = this.addMarkers(points);
+        this.searchMarkers = layer;
+        const extent = layer.getSource().getExtent();
+        this.map.getView().fit(extent, {
+          size: this.map.getSize(),
+          duration: 500,
+        });
       } catch (error) {
         console.log(error);
       }
     },
     setToken(token) {
-      this.api = createApi(token)
-    }
+      this.api = createApi(token);
+    },
+    changeMapType(type) {
+      this.map.setMapType(type);
+      this.mapType = type;
+    },
+    togglePoi(value) {
+      this.map.switchPoiLayer(value);
+    },
+    toggleTraffic(value) {
+      this.map.switchTrafficLayer(value);
+    },
   },
   mounted() {
-    this.setToken(this.serviceKey)
-    this.sanitizedCenter = this.sanitizeLocation(this.center);
+    this.setToken(this.serviceKey);
+    this.sanitizedCenter = sanitizeLocation(this.center);
     const scriptTag = this.importMap();
     scriptTag.onload = this.bundleMap;
   },
@@ -231,5 +328,60 @@ export default {
 
 #map {
   height: 100%;
+  position: relative;
+}
+
+#settings {
+  background-color: white;
+  border-radius: 0.5vw;
+  border: 1px solid rgb(178, 178, 178);
+  position: absolute;
+  z-index: 10;
+  bottom: 3.5vh;
+  max-width: 8.6vw;
+  max-height: 8vw;
+  overflow: hidden;
+  transition: 0.5s;
+}
+
+#settings:hover {
+  max-width: 100%;
+  max-height: 100%;
+  transition: 1s;
+}
+
+#checkboxes {
+  display: flex;
+  justify-content: space-around;
+  padding-top: 1vw;
+  padding-bottom: 1vw;
+}
+
+#tiles {
+  display: flex;
+}
+
+.tile {
+  cursor: pointer;
+}
+
+.tile img {
+  width: 8vw;
+  height: 8vw;
+  border-radius: 1vw;
+  margin-left: 0.3vw;
+  margin-right: 0.3vw;
+}
+
+.desc {
+  overflow: hidden;
+  padding-right: 0.1vw;
+  padding-left: 0.1vw;
+  text-align: center;
+  font-size: small;
+}
+
+.selected-tile {
+  border: 1px solid rgb(145, 145, 145);
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,81 @@
 <template>
-  <Map
-  mapKey="web.ApsMGWLRNZ6JAsKIKfVjhTfX5ojUSeSdk7kVuavm"
-  serviceKey="service.iEBKgNGr3yicBeQgKhFKB187X3df2vFmqpOLM5GD"
-  :center="{ latitude: 36.311559, longitude: 59.5870851}"
-  poi
-  traffic
-  />
+  <div class="page">
+    <div id="search-form">
+      <label>
+        <input type="checkbox" name="activate-search" v-model="autoSearch" @keydown.enter="search" />
+        :جستجو با کلیک روی نقشه
+      </label>
+      <br />
+      <label class="justify-between">
+        <input dir="rtl" type="search" name="search" v-model="searchText" @keydown.enter="search" />
+        :متن جستجو
+      </label>
+      <br />
+      <label class="justify-between">
+        <input type="text" name="coords" v-model="coords" />
+        :مختصات جستجو
+      </label>
+      <br />
+      <button @click="search">جستجو</button>
+    </div>
+    <Map
+      ref="map"
+      mapKey="web.ApsMGWLRNZ6JAsKIKfVjhTfX5ojUSeSdk7kVuavm"
+      serviceKey="service.iEBKgNGr3yicBeQgKhFKB187X3df2vFmqpOLM5GD"
+      :center="{ latitude: 36.311559, longitude: 59.5870851 }"
+      @click="handleClick"
+      poi
+      traffic
+    />
+  </div>
 </template>
 
 <script>
-import Map from '@/components/Map.vue'
+import Map from "@/components/Map.vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    Map
-  }
-}
+    Map,
+  },
+  data() {
+    return {
+      searchText: null,
+      autoSearch: false,
+      coords: null,
+    };
+  },
+  methods: {
+    handleClick(event) {
+      this.coords = event.stdPoint;
+      if (this.autoSearch) {
+        this.search();
+      }
+    },
+    search({ coords = this.coords, text = this.searchText } = {}) {
+      this.$refs.map.search({ text, coords });
+    },
+  },
+};
 </script>
+
+<style>
+.page {
+  height: 100%;
+}
+#search-form {
+  padding: 1rem;
+  position: absolute;
+  z-index: 2;
+  background-color: white;
+  border: 1px solid rgb(116, 116, 116);
+  min-width: 300px;
+  max-width: 50vw;
+  top: 4vh;
+  left: 4vw;
+}
+.justify-between {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
