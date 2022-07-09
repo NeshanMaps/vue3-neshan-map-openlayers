10dad83d32d181fdda031375b4b1d3cc62a4fa34
morteza gholami <mortezagholami1376.mg@gmail.com>
2022-07-09 12:50:02 +0430

    build: add squashed .d.ts file by typescript-declaration-webpack-plugin package

--- src/utils/markers.util.ts
+++ src/utils/markers.util.ts
@@ -76,7 +76,7 @@ export function markersFunc({ map, markersIconCallback }: MarkersMixinProps) {
   const createSource = (features: any): any => {
     return new ol.source.Vector({
       features,
-      projection: map.value.getView().projection // Is it neccesary?
+      projection: map.value.getView().projection, // Is it neccesary?
     });
   };
 
@@ -142,22 +142,28 @@ export function markersFunc({ map, markersIconCallback }: MarkersMixinProps) {
     // const styleFunc = styleFuncGen({ style: _style, hardText: showPopup })
     // const source = createSource(features);
     // const _pinLayer = createLayer({ style: styleFunc, source });
-    const clusterLayer = createClusterLayer(features, showPopup)
+    const clusterLayer = createClusterLayer(features, showPopup);
     return { layer: clusterLayer, style: _style };
   };
 
-  const styleFuncGen = ({ style, hardText = false }: { style: any, hardText?: boolean }) => {
+  const styleFuncGen = ({
+    style,
+    hardText = false,
+  }: {
+    style: any;
+    hardText?: boolean;
+  }) => {
     return (feature: any) => {
       if (hardText) {
         style.getText().setText(feature.get("text"));
       }
-      const iconProps = feature.get("iconProps")
+      const iconProps = feature.get("iconProps");
       if (iconProps) {
         style.setImage(createIcon(feature.get("iconProps")));
       }
       return style;
     };
-  }
+  };
 
   /**
    * Receives an array of points and marks them on map.
@@ -190,19 +196,19 @@ export function markersFunc({ map, markersIconCallback }: MarkersMixinProps) {
     return new ol.source.Cluster({
       distance: 30,
       minDistance: 30,
-      source: createSource(features)
-    })
-  }
+      source: createSource(features),
+    });
+  };
 
   const createClusterText = (size: number) => {
     return new ol.style.Text({
       text: size.toString(),
-      font: 'bold 15px serif',
+      font: "bold 15px serif",
       fill: new ol.style.Fill({
         color: [0, 0, 0, 1],
       }),
-    })
-  }
+    });
+  };
 
   const createClusterCircle = () => {
     return new ol.style.Circle({
@@ -214,15 +220,15 @@ export function markersFunc({ map, markersIconCallback }: MarkersMixinProps) {
       fill: new ol.style.Fill({
         color: [0, 153, 255, 0.7],
       }),
-    })
-  }
+    });
+  };
 
   const createClusterStyle = (size: number) => {
     return new ol.style.Style({
       image: createClusterCircle(),
       text: createClusterText(size),
-    })
-  }
+    });
+  };
 
   /**
    * Creates a style function for the clusters.
@@ -230,38 +236,49 @@ export function markersFunc({ map, markersIconCallback }: MarkersMixinProps) {
    * the icon shown is the icon for single markers.
    * @param styleCache - An object to remember the set styles
    * @param showPopup - whether it should show a popup on hover or is a static text
-   * @returns 
+   * @returns
    */
-  const createClusterStyleFunc = (styleCache: { [s: string]: any }, showPopup: boolean) => {
+  const createClusterStyleFunc = (
+    styleCache: { [s: string]: any },
+    showPopup: boolean
+  ) => {
     return (clusterFeature: any) => {
-      const innerFeatures = clusterFeature.get('features')
-      clusterFeature.set('text', innerFeatures.map((feat: any) => feat.get('text')))
+      const innerFeatures: any[] = clusterFeature.get("features");
+      clusterFeature.set(
+        "text",
+        innerFeatures.map((feat: any) => feat.get("text"))
+      );
       const size = innerFeatures.length;
       let style = styleCache[size];
       if (!style) {
         if (size !== 1) {
+          // Show cluster marker on multiple features
           style = createClusterStyle(size);
           styleCache[size] = style;
         } else {
+          // Show single marker on single feature
           const _text = createText();
-          const _image = createIcon({ color: 'blue', iconScale: 0.15 });
+          const _image = createIcon({ color: "blue", iconScale: 0.15 });
           const _style = createStyle({ text: _text, image: _image });
-          const styleFunc = styleFuncGen({ style: _style, hardText: !showPopup })
-          style = styleFunc(innerFeatures[0])
+          const styleFunc = styleFuncGen({
+            style: _style,
+            hardText: !showPopup,
+          });
+          style = styleFunc(innerFeatures[0]);
         }
       }
       return style;
-    }
-  }
+    };
+  };
 
   const createClusterLayer = (features: any[], showPopup: boolean) => {
     const styleCache: { [s: string]: any } = {};
     const cluster_layer = new ol.layer.Vector({
       source: createClusterSource(features),
-      style: createClusterStyleFunc(styleCache, showPopup)
+      style: createClusterStyleFunc(styleCache, showPopup),
     });
-    return cluster_layer
-  }
+    return cluster_layer;
+  };
 
   return {
     createText,
