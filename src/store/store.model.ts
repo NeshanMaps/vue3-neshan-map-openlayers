5f2ec3d5311f0bdd095b080d3f51f2d333d35e95
Morteza Gholami <mortezagholami1376.mg@gmail.com>
2022-10-11 12:57:00 +0330

    remove unneccesary diles

--- src/store/store.model.ts
+++ src/store/store.model.ts
@@ -12,7 +12,7 @@ export declare interface Context {
   getters: any
 }
 
-type Func = (...args: any) => any
+type Func = (context: Context, ...args: any) => any
 type ActionsModuleMapper<AM extends { [s: string]: { [y: string]: Func } }> = {
   [Module in keyof AM]: ActionsMapper<AM[Module]>
 }
@@ -21,8 +21,10 @@ type ActionsMapper<Module extends { [s: string]: Func }> = {
   [A in keyof Module]: ChangedFunction<Module[A]>
 }
 
-type ChangedFunction<F extends Func> = Parameters<F>[1]
+type ChangedFunction<F extends Func> = (...args: Parameters<F>) => void
 
 type MappedActions = ActionsModuleMapper<Actions>
 
 type test = ActionsMapper<Actions["map"]>
+
+type funcTest = ChangedFunction<Actions["map"]["setMapType"]>
