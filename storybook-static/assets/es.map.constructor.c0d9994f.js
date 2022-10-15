9c1228a4af27b32648c31ab42d3018be8878ae7e
Morteza Gholami <mortezagholami1376.mg@gmail.com>
2022-10-15 12:00:42 +0330

    docs: remove storybook-static and add it to git ignore

--- storybook-static/assets/es.map.constructor.c0d9994f.js
+++
@@ -1,2 +0,0 @@
-import{h as t,i as o}from"./iframe.5b3192c5.js";var r=t,e=o;r("Map",function(n){return function(){return n(this,arguments.length?arguments[0]:void 0)}},e);
-//# sourceMappingURL=es.map.constructor.c0d9994f.js.map
